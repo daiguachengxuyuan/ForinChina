49cde12a6a3b1bc75a57e44e7d77f8ec2dba681d
root <root@localhost.localdomain>
2015-01-04 04:47:49 +0800

    2015/01/03

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Welcome to ForinChina</title>
+<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+<?php include "includes/head.php" ?>
+<div id="content">
+ <div id="indexImg">
+  <img width=626 height=440 src="images/indexImg_Chinamap.jpg">
+ </div>
+ <div id="loginBlk">
+  <div id="login">
+   <div class="logInput"><label>login</label></div>
+   <div class="logInput"><label>username:</label><input class="loginp" type="text" name="userName"></div>
+   <div class="logInput"><label>password:</label><input class="loginp" type="password" name="passWorld"></div>
+   <div class="logInput"><label>verifycd:</label><input class="loginp" type="test" name="submit"></div>   
+   <div class="logInput"><input type="submit" name="submit" value="submit"></div>
+  </div>
+ </div>
+</div>
+
+<div id="footer">
+</div>
+</body>
+</html>
